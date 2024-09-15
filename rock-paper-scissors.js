ee64406b688d5ecbdbd73e389df2ea0dfd5dc700
Osita <101451314+oceeta@users.noreply.github.com>
2024-09-15 15:24:32 +0100

    Add function to get the computer's choice

---
+++ rock-paper-scissors.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    switch (randomNumber) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+    }
+}
