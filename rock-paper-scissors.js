6f4a12801cbe1770a2aab0cff70264c4311a5492
Osita <101451314+oceeta@users.noreply.github.com>
2024-09-15 16:03:04 +0100

    Add function to get human choice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -9,3 +9,26 @@ function getComputerChoice() {
             return "scissors";
     }
 }
+
+function getHumanChoice() {
+    // get the human player's choice and store it in a variable called choice
+    let choice = prompt("Please enter your choice between either \"rock\", \"paper\", or \"scissors\"", "");
+
+    // convert the choice to lowercase and return "rock", "paper", or "scissors" depending on the player's choice
+    switch (choice.toLowerCase()) {
+        case "rock":
+            return "rock";
+        case "paper":
+            return "paper";
+        case "scissors":
+            return "scissors";
+
+    // continue to prompt the user for a valid choice until they either provide one or cancel
+        case "":
+            return getHumanChoice();    
+        default:
+            return getHumanChoice();
+    }
+}
+
+console.log(getHumanChoice());
